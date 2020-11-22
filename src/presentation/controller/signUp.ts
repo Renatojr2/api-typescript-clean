8fe9c0d4dc77347a2a1b89a3505a1a07b6b6d6c9
Renato Junior <49106675+Renatojr2@users.noreply.github.com>
2020-11-22 12:31:57 -0300

    refactory: function badrequest

--- src/presentation/controller/signUp.ts
+++ src/presentation/controller/signUp.ts
@@ -1,13 +1,14 @@
 import { HttpRequest, HttpResponse } from '../protocols/http';
 import { MissingParamErro } from '../erros/missing-param-erro';
+import { badRequest } from '../helpers/http-helper';
 
 export class SignUpController {
   handle(httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
-      return { statusCode: 400, body: new MissingParamErro('name') };
+      return badRequest(new MissingParamErro('name'));
     }
     if (!httpRequest.body.email) {
-      return { statusCode: 400, body: new MissingParamErro('email') };
+      return badRequest(new MissingParamErro('email'));
     }
   }
 }

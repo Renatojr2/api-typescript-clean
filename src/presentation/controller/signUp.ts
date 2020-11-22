5009b14411b7debb34761ae03f8dafd4cb2ef79f
Renato Junior <49106675+Renatojr2@users.noreply.github.com>
2020-11-22 12:48:03 -0300

    refactor: function badrequest

--- src/presentation/controller/signUp.ts
+++ src/presentation/controller/signUp.ts
@@ -1,8 +1,9 @@
+import { Controller } from '../protocols/controller';
 import { HttpRequest, HttpResponse } from '../protocols/http';
 import { MissingParamErro } from '../erros/missing-param-erro';
 import { badRequest } from '../helpers/http-helper';
 
-export class SignUpController {
+export class SignUpController implements Controller {
   handle(httpRequest: HttpRequest): HttpResponse {
     const requiredFields = [
       'name',
